0c50a8089e897c30ad178df7cfcde79209dca7a1
glen <glen.sanjose@hotmail.ca>
2021-03-29 18:03:48 -0400

    changed and added functions for hamburger icon change and changed layout for skills div

--- app.js
+++ app.js
@@ -1,43 +1,30 @@
 const mySite = {}
 
+mySite.hamburger = document.querySelector('#menu-button');
+const hamburger = mySite.hamburger;
+
 mySite.photoChange = () => {
    const myPhoto = document.querySelector('.self-container');
    myPhoto.addEventListener('click', () => {
-      // if (event.target.src.match('./images/glen-portrait.png')) {
-      //    event.target.src = './images/glensq.jpg';
-      // } else {
-      //    event.target.src = './images/glen-portrait.png';
-      // }
       const drawing = document.querySelector('#self-drawing');
       const photo = document.querySelector('#self-pic');
-
       drawing.classList.toggle('shown');
       photo.classList.toggle('hidden');
    })
 }
 
 mySite.showNav = () => {
-   const hamburger = document.querySelector('#menu-button');
-   hamburger.addEventListener('click', () => {
-      if (hamburger.innerHTML.match(`<i class="fas fa-times"></i>`)) {
-         hamburger.innerHTML = `<i class="fas fa-bars"></i>`;
-      } else {
-         hamburger.innerHTML = `<i class="fas fa-times"></i>`;
-      }
+   mySite.hamburger.addEventListener('click', () => {
+   mySite.changeIcon();
    const nav = document.querySelector('#nav-menu');
    nav.classList.toggle('show-menu');
    })
 }
 
 mySite.keyShowNav = () => {
-   const hamburger = document.querySelector('#menu-button');
-   hamburger.addEventListener('keypress', (event) => {
+   mySite.hamburger.addEventListener('keypress', (event) => {
       if (event.key == 'Enter') {
-         if (hamburger.innerHTML.match(`<i class="fas fa-times"></i>`)) {
-            hamburger.innerHTML = `<i class="fas fa-bars"></i>`;
-         } else {
-            hamburger.innerHTML = `<i class="fas fa-times"></i>`;
-         }
+         mySite.changeIcon();
          const nav = document.querySelector('#nav-menu');
          nav.classList.toggle('show-menu');
       }
@@ -47,8 +34,17 @@ mySite.keyShowNav = () => {
 mySite.hideMenu = () => {
    const nav = document.querySelector('#nav-menu');
    nav.addEventListener('click', () => {
-      nav.classList.toggle('show-menu');
+      nav.classList.toggle('show-menu')
+      mySite.changeIcon();
    })
+}
+
+mySite.changeIcon = () => {
+   if (hamburger.innerHTML.match(`<i class="fas fa-times"></i>`)) {
+      hamburger.innerHTML = `<i class="fas fa-bars"></i>`;
+   } else {
+      hamburger.innerHTML = `<i class="fas fa-times"></i>`;
+   }
 }
 
 
